cb79e8b5c08cd8644c43363977025a022d8c64d2
Tomas I. Opazo <toopazo@protonmail.com>
2021-09-16 14:09:47 -0400

    refactor logger

---
+++ ars_interface.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+# import time
+import serial
+import numpy as np
+import pprint
+# import io
+# import signal
+# import csv
+# import sys
+import threading
+
+
+class ArsIface:
+    def __init__(self):
+        self.serial = serial.Serial(
+            port='/dev/ttyACM0',
+            baudrate=115200,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1
+        )
+
+        self.serial_data = None
+        self.serial_thread_lock = threading.Lock()
+        self.serial_thread_event = threading.Event()
+        self.serial_thread = threading.Thread(
+            target=self.serial_thread_main, args=(self.serial_thread_event,))
+        self.serial_thread.start()
+        # self.thread1.join()
+
+    def serial_thread_main(self, close_event):
+        assert isinstance(close_event, threading.Event)
+        while not close_event.is_set():
+            line = self.serial.readline()
+            data = ArsIface.parse_line(line)
+            self.safely_write_data(data)
+        self.serial.close()
+
+    def serial_thread_close(self):
+        self.serial_thread_event.set()
+        # blocks the calling thread until the thread whose join() method is
+        # called is terminated.
+        self.serial_thread.join()
+
+    def safely_read_data(self):
+        if self.serial_thread_lock.locked():
+            print('[safely_read_data] lock is taken, we need to wait ..')
+        self.serial_thread_lock.acquire()
+        var = self.serial_data
+        self.serial_thread_lock.release()
+        return var
+
+    def safely_write_data(self, data):
+        if self.serial_thread_lock.locked():
+            print('[safely_write_data] lock is taken, we need to wait ..')
+        self.serial_thread_lock.acquire()
+        self.serial_data = data
+        self.serial_thread_lock.release()
+
+    @staticmethod
+    def parse_line(line):
+        # b'100,75000,75.000,1: 0,0,0,0,0,0,0,0,418,413,407,401,396,390,384,397,\r\n'
+        # line = header:data
+        # header = cnt, tms, tms/1000, mills()-tms
+        # data = s0,s1,s2,s3,s4,s5,s6,s7,s8,s8,s10,s11,s12,s13,s14,s15
+
+        # print(type(line))
+        line = line.decode("utf-8")
+        # print(type(line))
+        assert isinstance(line, str)
+        hd_sep = ':'
+        cm_sep = ','
+        if hd_sep not in line:
+            return {'Error': '[parse_line] unrecognized format'}
+        else:
+            hd = line.split(hd_sep)
+            # print(hd)
+            header = hd[0]
+            data = hd[1]
+            h = header.split(cm_sep)
+        if len(h) != 4:
+            return {'Error': '[parse_line] header fields do not match'}
+        else:
+            header_dict = {'sps': int(h[0]), 'mills': int(h[1]),
+                           'secs': float(h[2]), 'dtmills': int(h[3])}
+        data = data.split(cm_sep)
+        data.pop()  # get rid of '\r\n'
+        data = np.array(data, np.int)
+        return {'header': header_dict, 'data': data}
--- ars_main.py
+++ ars_main.py
@@ -1,90 +1,50 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
-# import time
-import serial
-import numpy as np
-import pprint
-# import io
-import signal
-import csv
 import sys
+# import numpy as np
+# import signal
+# import time
+# import datetime
+from toopazo_tools.file_folder import FileFolderTools as FFTools
+from toopazo_tools.telemetry import TelemetryLogger
+# from kdecan_interface import KdeCanIface
+from ars_interface import ArsIface
 
 
-class ArsWriter:
-    def __init__(self, filename):
-        self.ser = serial.Serial(
-            port='/dev/ttyACM0',
-            baudrate=115200,
-            parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE,
-            bytesize=serial.EIGHTBITS,
-            timeout=1
-        )
-        self.pp = pprint.PrettyPrinter(indent=4, width=120, compact=True)
-        self.log_fd = open(filename, 'w', encoding="utf-8")
-        self.log_csv_writer = csv.writer(self.log_fd)
-        # self.log_csv_writer.writerow(fields)
-        self.pp.pprint(type(self.log_fd))
-        self.sigint = False
+class ArsIfaceWrapper:
+    def __init__(self):
+        self.ars = ArsIface()
+        self.esc_arr = list(range(11, 19))
 
-    def run(self):
-        while not self.sigint:
-            x = self.ser.readline()
-            # print(x)
-            y = ArsWriter.parse_line(x)
-            self.pp.pprint(y)
-            self.write_to_file(y)
-        self.log_fd.close()
+    def get_data(self):
+        log_data = self.ars.safely_read_data()
+        return log_data
 
-    def sigint_handler(self, _signal, _frame):
-        self.pp.pprint('[sigint] signal.SIGINT (ID: {}) has been caught'.format(_signal))
-        self.sigint = True
+    def close(self):
+        self.ars.serial_thread_close()
 
-    def write_to_file(self, xdict):
-        # aassert isinstance(fd, io.FileIO)
-        # fd.writelines([xdict])
-        mkey = 'data'
-        if mkey in xdict.keys():
-            # datastr = self.pp.pformat(xdict[mkey])
-            # datastr = str(xdict[mkey])
-            # self.fd.write(datastr + '\r\n')
-            self.log_csv_writer.writerow(xdict[mkey])
-        else:
-            pass
 
-    @staticmethod
-    def parse_line(line):
-        # b'100,75000,75.000,1: 0,0,0,0,0,0,0,0,418,413,407,401,396,390,384,397,\r\n'
-        # line = header:data
-        # header = cnt, tms, tms/1000, mills()-tms
-        # data = s0,s1,s2,s3,s4,s5,s6,s7,s8,s8,s10,s11,s12,s13,s14,s15
-
-        # print(type(line))
-        line = line.decode("utf-8")
-        # print(type(line))
-        assert isinstance(line, str)
-        hd_sep = ':'
-        cm_sep = ','
-        if hd_sep not in line:
-            return {'Error': '[parse_line] unrecognized format'}
-        else:
-            hd = line.split(hd_sep)
-            # print(hd)
-            header = hd[0]
-            data = hd[1]
-            h = header.split(cm_sep)
-        if len(h) != 4:
-            return {'Error': '[parse_line] header fields do not match'}
-        else:
-            header_dict = {'sps': int(h[0]), 'mills': int(h[1]),
-                           'secs': float(h[2]), 'dtmills': int(h[3])}
-        data = data.split(cm_sep)
-        data.pop()  # get rid of '\r\n'
-        data = np.array(data, np.int)
-        return {'header': header_dict, 'data': data}
+def parse_user_arg(folder):
+    folder = FFTools.full_path(folder)
+    print('target folder {}'.format(folder))
+    if FFTools.is_folder(folder):
+        cwd = FFTools.get_cwd()
+        print('current folder {}'.format(cwd))
+    else:
+        arg = '{} is not a folder'.format(folder)
+        raise RuntimeError(arg)
+    return folder
 
 
 if __name__ == '__main__':
-    ardusensor = ArsWriter(sys.argv[1])
-    signal.signal(signal.SIGINT, ardusensor.sigint_handler)
-    ardusensor.run()
+    telem_folder = parse_user_arg(sys.argv[1])
+    telem_iface = ArsIfaceWrapper()
+    telem_ext = ".ars"
+    telem_logger = TelemetryLogger(telem_folder, telem_iface, telem_ext)
+
+    usampling_period = 0.1
+    ulog_header = "time s, escid, "\
+                  "voltage V, current A, angVel rpm, temp degC, warning, " \
+                  "inthtl us, outthtl perc"
+    telem_logger.live_data(usampling_period, ulog_header)
+
